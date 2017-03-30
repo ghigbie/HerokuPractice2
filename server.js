0cfe8e62e856cbc852952d822d203987773d76fb
George Higbie <gehigbie@gmail.com>
2017-03-29 17:09:28 -0700

    fix typo

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ var app = express();
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
 
-// app.use((req, res, next) = > {
-//     var now = new Date().toString();
-//     var log = `${now}: ${req.method} | ${req.path}`;
-//     console.log(log);
-//     fs.appendFile("server.log", log + "\n", (err) =>{
-//         if(err){
-//             console.log("Unable to append to server.log");
-//         }
-//     });
-//     next();
-// });
+app.use((req, res, next) => { //this is middleware
+    var now = new Date().toString();
+    var log =`${now}: ${req.method} | ${req.path}`;
+    console.log(log);
+    fs.appendFile("server.log", log + "\n", (err) => {
+        if(err){
+            console.log("Unable to append to server.log");
+        }
+    });
+    next();
+});
 
 // app.use((req, res) => {
 //     res.render("maintenance.hbs", {
